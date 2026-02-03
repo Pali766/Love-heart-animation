1b7f089921b8c6f82a168e971fd78b8c98430536
Pali766 <pgebrizsolt@gmail.com>
2026-02-03 10:13:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const canvas = document.getElementById("canvas");
-const ctx
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const text = "I LOVE YOU";
+const particles = [];
+const heartParticles = [];
+
+// Betűk részecskéi
+ctx.fillStyle = "white";
+ctx.font = "bold 100px Arial";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillText(text, canvas.width/2, canvas.height/2);
+
+const imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+
+for(let y=0;y<canvas.height;y+=6){
+  for(let x=0;x<canvas.width;x+=6))
